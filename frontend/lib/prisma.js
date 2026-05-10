07e79aa0571f28e4171f52697933252051e02156
Yuvraj Singh <13yuvrajsingh2004@gmail.com>
2026-05-10 12:29:03 +0530

    Fix Prisma client caching in development

--- frontend/lib/prisma.js
+++ frontend/lib/prisma.js
@@ -5,9 +5,7 @@ import { PrismaClient } from '@prisma/client';
  */
 const globalForPrisma = global;
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
+export const prisma = new PrismaClient({
     log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
   });
 

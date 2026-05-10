f009aad96359f8061de08fa912cf11d50eb046d3
Yuvraj Singh <13yuvrajsingh2004@gmail.com>
2026-05-10 12:29:07 +0530

    Remove NotificationBell from global dashboard layout

--- frontend/app/(dashboard)/layout.js
+++ frontend/app/(dashboard)/layout.js
@@ -3,7 +3,6 @@ import NotificationBell from '@/components/NotificationBell';
 export default function DashboardLayout({ children }) {
   return (
     <div>
-      <NotificationBell />
       {children}
     </div>
   );
